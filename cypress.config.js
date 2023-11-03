53c1aea9dc0383db34b6a8c4caf610fa6a62a662
Miroslav Kadidlo <kadidlo.miroslav@gmail.com>
2023-11-03 13:47:46 +0100

    feat(parsing): added parsing option

--- cypress.config.js
+++ cypress.config.js
@@ -15,26 +15,26 @@ module.exports = defineConfig({
       on('after:spec', (spec, results) => {
         if (results) {
           console.log('after:spec hook was called')
+          fs.readFile('./results/my-test-output.xml', 'utf8', function(err, xmlData) { 
+            if (err) {
+              console.log('err')
+            } else {
+              // Display the file content 
+              console.log(xmlData)
+              console.log('xmlData has been readed.')
 
-          function parse() {
-            fs.readFile('./results/my-test-output.xml', 'utf8', function(err, xmlData) { 
-              if (err) {
-                console.log(err)
-              } else {
-                // Display the file content 
-                console.log(xmlData)
-                console.log('xmlData has been readed.')
-
-                const parser = new XMLParser()
-                let jObj = parser.parse(xmlData)
-
-                console.log(jObj)
+              const options = {
+                ignoreAttributes: false
               }
-            })
-          }
 
-          parse()
+              const parser = new XMLParser(options)
+              let jObj = parser.parse(xmlData)
 
+              console.log(jObj)
+
+              fs.writeFileSync('./results/my-test-output.json', JSON.stringify(jObj, null, 2) , 'utf-8')
+            }
+          })
         }
       })
     }
